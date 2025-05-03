a76c3e80984ed957bd04afa03d1db26fdc8a3ca5
iababio <iba-melch@hotmail.com>
2025-05-02 20:27:36 -0400

    prettier check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,48 @@
-import React from "react";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
-import { Inter, Epilogue } from "next/font/google";
+import React from 'react';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter, Epilogue } from 'next/font/google';
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  display: "swap",
+   variable: '--font-inter',
+   subsets: ['latin'],
+   weight: ['400', '500', '700'],
+   display: 'swap',
 });
 
 const epilogue = Epilogue({
-  variable: "--font-epilogue",
-  subsets: ["latin"],
-  weight: "500",
-  display: "swap",
+   variable: '--font-epilogue',
+   subsets: ['latin'],
+   weight: '500',
+   display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "Language Platform",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
-  },
-  description: "Train a custom language model",
+   title: 'Language Platform',
+   icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon.ico',
+      apple: '/apple-touch-icon.png',
+   },
+   description: 'Train a custom language model',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={`${inter.variable} ${epilogue.variable}`}>
-      <head />
-      <body className="min-h-screen bg-background font-sans antialiased">
-        <ClerkProvider
-          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-          signInUrl="/sign-in"
-          signUpUrl="/sign-up"
-          afterSignInUrl="/chat"
-          afterSignUpUrl="/chat"
-        >
-          {children}
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <html lang="en" className={`${inter.variable} ${epilogue.variable}`}>
+         <head />
+         <body className="min-h-screen bg-background font-sans antialiased">
+            <ClerkProvider
+               publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+               signInUrl="/sign-in"
+               signUpUrl="/sign-up"
+               afterSignInUrl="/chat"
+               afterSignUpUrl="/chat"
+            >
+               {children}
+            </ClerkProvider>
+         </body>
+      </html>
+   );
 }
